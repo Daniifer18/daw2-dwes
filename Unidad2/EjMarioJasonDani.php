d1d9584a45224f27028470034452c22fb10d58fd
Daniifer18 <danportero6334@gmail.com>
2022-10-07 17:43:21 +0200

    fin

--- Unidad2/EjMarioJasonDani.php
+++ Unidad2/EjMarioJasonDani.php
@@ -1,21 +1,129 @@
 <?php
 
 $productos = [
-    "naranja" => 1.2,
-    "manzana" => 1.5,
-    "pera" => 1.8,
-    "platano" => 0.8,
-    "kiwi" => 0.75,
-    "macarrones" => 0.5,
-    "arroz" => 0.75,
-    "salchichas" => 1,
-    "patatas_fritas" => 3,
-    "paninis" => 1.5,
-    "leche_6_uds" => 5,
-    "pizza_jamon_serrano" => 2.59,
-    "helado_chocolate" => 2.99
+    "Naranja" => 1.2,
+    "Manzana" => 1.5,
+    "Pera" => 1.8,
+    "Platano" => 0.8,
+    "Kiwi" => 0.75,
+    "Macarrones" => 0.5,
+    "Arroz" => 0.75,
+    "Salchichas" => 1,
+    "Patatas_fritas" => 3,
+    "Paninis" => 1.5,
+    "Leche_6_uds" => 5,
+    "Pizza_jamon_serrano" => 2.59,
+    "Helado_chocolate" => 2.99
 ];
+/*Funcion del array que me devuelve las claves del array*/
+$producto = array_keys($productos);
+/*Funcion del array que me devuelve los valores del array*/
+$precio = array_values($productos);
 
-
-
+$subtotal = 0;
 ?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Lista de la compra</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <style>
+            body{
+                background-color: lightgrey;
+            }
+            #centro{
+                margin-top: 2%;
+            }
+            table{
+                width: 100%;
+                border-radius: 3px;
+                background-color: white;
+                box-shadow: 10px 5px 5px grey;
+            }
+            th,#total{
+                background-color: blue;
+                color: white;
+            }
+            .ho{
+                border-radius: 3px;
+            }
+            .ho:hover{
+                background-color: lightgreen;
+            }
+            .cab{
+                text-align: center;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="row">
+            <div class="col-md-3"></div>
+            <div class="col-md-6" id="centro">
+                <div>
+                    <form  method="GET" action="EjMarioJasonDani.php">
+                        <table>
+                            <th colspan="3" class="cab">
+                                --Lista de productos--
+                            </th>
+                            <?php for($i = 0; $i < count($productos); $i++) { ?>
+                                <tr>
+                                    <td id="poduct" class="ho">
+                                        <?php echo $producto[$i]?>
+                                    </td>
+                                    <td id="price" class="ho">
+                                        <?php echo $precio[$i]?>
+                                    </td>
+                                    <td id="cantidad" class="ho">
+                                        <input type="number" value=0 min=0 max=100 name="<?php echo $producto[$i] ?>">
+                                    </td>
+                                </tr>
+                            <?php } ?>
+                            <tr>
+                                <td colspan="3" class="cab">
+                                    <input type="submit" value="Generar factura" class="btn btn-primary btn-block">
+                                </tr>
+                            </tr>
+                        </table>
+                    </form>
+                </div>
+                <br><br>
+                <?php if(isset($_GET[$producto[0]])){ ?>
+                    <div>
+                        <table id="factura">
+                            <th>Producto</th>
+                            <th>Precio</th>
+                            <th>Total</th>
+                            <?php for ($i=0; $i < count($producto); $i++) { ?>
+                                <?php if(isset($_GET[$producto[$i]]) && $_GET[$producto[$i]] != 0){ ?>
+                                    <tr>
+                                        <td id="poduct" class="ho">
+                                            <?php echo $producto[$i]?>
+                                        </td>
+                                        <td id="price" class="ho">
+                                            <?php echo $precio[$i]?>
+                                        </td>
+                                        <td id="cantidad" class="ho">
+                                            <?php $subtotal += $_GET[$producto[$i]] * $precio[$i]; 
+                                            echo $_GET[$producto[$i]] * $precio[$i] ?>
+                                        </td>
+                                    </tr>
+                                <?php } ?>
+                            <?php } ?>   
+                            <tr id="total">
+                                <td>
+                                    Precio total:
+                                </td>
+                                <td colspan="2" class="cab">
+                                    <?= $subtotal ?>
+                                </td>
+                            </tr>         
+                        </table>
+                    </div>
+                <?php } ?>    
+            <div class="col-md-3"></div>
+        </div>
+    </body>
+</html>
---
+++ Unidad2/EjNacho.php
@@ -0,0 +1,16 @@
+<?php 
+
+/*
+array_rand(array $array, int $num = 1): mixed
+array: El array de entrada.
+num: Especifica cuántas entradas deberían seleccionarse.
+
+Dados dos arrays unidimensionales, uno de tareas[] y otro de personas[], 
+asigna de manera aleatoria una tarea a cada persona. Si ya le echas valor, crea un arraybidimensional 
+de tareas_diarias[dia][tarea] y haz un organigrama donde asignes tareas a cada persona durante la semana
+*/
+
+$tareas = ["Barrer","Fregar","Comer","Dormir"];
+$personas = ["Jose","Pepe","Ana","Maria"];
+
+?>
