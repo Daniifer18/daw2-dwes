55f8410dec9913ae4b2e4a615a6db6dc83eb4410
Daniifer18 <danportero6334@gmail.com>
2022-10-13 21:13:06 +0200

    fin

--- Unidad3/Ej6.php
+++ Unidad3/Ej6.php
@@ -25,7 +25,14 @@ function sumaNumeros($n1,$n2){
         <!--Varias llamadas-->
         <?php for($i = 0;$i < 10;$i++) { ?>
             <h3>
-                <?= sumaNumeros($n1,$n2) ?>
+                <?php 
+                $n1 = mt_rand(1,20); $n2 = mt_rand(1,20);$vaux = 0;
+                if($n1 > $n2){
+                    $vaux = $n2;
+                    $n2 = $n1;
+                    $n1 = $vaux;
+                } 
+                echo sumaNumeros($n1,$n2) ?>
             </h3>
             <?php } ?>
     </body>
